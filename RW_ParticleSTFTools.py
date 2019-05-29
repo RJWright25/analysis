37f1afe78687af05b490b7972b4e81dc62e73bdf
Ruby Wright <21486778@student.uwa.edu.au>
2019-05-29 11:22:01 +0800

    Small changes

--- RW_ParticleSTFTools.py
+++ RW_ParticleSTFTools.py
@@ -63,16 +63,13 @@ def read_sim_timesteps(run_directory,sim_type='SWIFT',snap_no=200,files_lz=4):
         snaps=[i for i in range(snap_no)]
         particle_data_file_directories=[run_directory+prefix+str(snap).zfill(files_lz)+".hdf5" for snap in snaps]
 
-        sim_timesteps={'Redshift':[],'Scale_factor':[]}
+        sim_timesteps={'Redshift':np.zeros(snap_no),'Scale_factor':np.zeros(snap_no)}
         fields_out=list(sim_timesteps.keys())
         
         for snap in snaps:
             particle_file_temp=h5py.File(particle_data_file_directories[snap])
             for ifield,field in enumerate(fields):
-                print(list(particle_file_temp['Header'].attrs),fields_out[ifield])
-                print(particle_file_temp['Header'].attrs[field])
-                print(sim_timesteps[fields_out[ifield]])
-                sim_timesteps[fields_out[ifield]].extend(particle_file_temp['Header'].attrs[field])
+                sim_timesteps[fields_out[ifield]][snap]=particle_file_temp['Header'].attrs[field]
             particle_file_temp.close()
 
         sim_timesteps['Lookback_time']=cosmo.lookback_time(sim_timesteps['Redshift'])
