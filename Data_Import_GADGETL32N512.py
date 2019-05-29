3a86f6ae93ee59fd84950e9c3da82cee41d9de7f
Ruby Wright <21486778@student.uwa.edu.au>
2019-05-29 13:36:35 +0800

    Small changes

--- Data_Import_GADGETL32N512.py
+++ Data_Import_GADGETL32N512.py
@@ -28,13 +28,7 @@ sim_mdm=particle_file_temp['Header'].attrs['MassTable'][1]
 sim_mgas=particle_file_temp['Header'].attrs['MassTable'][0]
 sim_fb=sim_mgas/(sim_mdm+sim_mgas)
 
-# Import BASE halo data + particle lists
-
 sim_timesteps=read_sim_timesteps(run_directory=run_directory,sim_type='GADGET',snap_no=201,files_lz=3)
-
-# Add delta_m0 and delta_m1 to each halo -- different trimming, different snap smoothing
-create_new=True
-read_data=True
 
 ######## halo_data keys ----
 
@@ -69,53 +63,22 @@ read_data=True
 # 'delta_m0_dt'
 # 'delta_m1_dt'
 
+# Import BASE halo data + particle lists
 
-if read_data:
-    if create_new:
-        halo_data_base=read_vr_treefrog_data(vr_directory=vr_directory,vr_prefix="snapshot_",tf_name=tf_treefile,snap_no=sim_no_snaps,files_lz=3,files_type=2,files_nested=True,extra_halo_fields=['npart'],verbose=1)
-        with open('halo_data_base.txt', 'wb') as halo_data_file:
+if False:#if we already have base halo data (nopl)
+    with open('halo_data_base_nopl.txt', 'rb') as halo_data_file:
+        halo_data_base_nopl = pickle.load(halo_data_file)
+        halo_data_file.close()
+    halo_data_base=read_vr_treefrog_data(vr_directory=vr_directory,vr_prefix="snapshot_",halo_data_all=halo_data_base_nopl,tf_name=tf_treefile,snap_no=sim_no_snaps,files_lz=3,files_type=2,files_nested=True,extra_halo_fields=['npart'],verbose=1,part_data_from_snap=180)
+    #once read, save this. 
+    with open('halo_data_base.txt', 'wb') as halo_data_file:
             pickle.dump(halo_data_base, halo_data_file)
             halo_data_file.close()
-
-    else:
-        with open('halo_data_base.txt', 'rb') as halo_data_file:
-            halo_data_base = pickle.load(halo_data_file)
+            
+else:#if we DONT already have base halo data (nopl)
+    halo_data_base=read_vr_treefrog_data(vr_directory=vr_directory,vr_prefix="snapshot_",halo_data_all=halo_data_base_nopl,tf_name=tf_treefile,snap_no=sim_no_snaps,files_lz=3,files_type=2,files_nested=True,extra_halo_fields=['npart'],verbose=1,part_data_from_snap=180)
+    #once read, save this. 
+    with open('halo_data_base.txt', 'wb') as halo_data_file:
+            pickle.dump(halo_data_base, halo_data_file)
             halo_data_file.close()
-
-    if create_new:
-        halo_data=gen_delta_npart(halo_data=halo_data_base,sim_timesteps=sim_timesteps,type_order=-1,mass_table=[sim_mdm,sim_mgas],snaps=list(range(121,200,1)),trim_hoes=True,depth=1)
-        with open('halo_data_appended_trimmed_d1.txt', 'wb') as halo_data_file:
-            pickle.dump(halo_data, halo_data_file)
-            halo_data_file.close()
-
-    else:
-        with open('halo_data_appended_trimmed_d1.txt', 'rb') as halo_data_file:
-            halo_data_appended_trimmed_d1 = pickle.load(halo_data_file)
-            halo_data_file.close()
-
-    if create_new:
-        halo_data=gen_delta_npart(halo_data=halo_data_base,sim_timesteps=sim_timesteps,type_order=-1,mass_table=[sim_mdm,sim_mgas],snaps=list(range(121,200,1)),trim_hoes=False,depth=1)
-        with open('halo_data_appended_d1.txt', 'wb') as halo_data_file:
-            pickle.dump(halo_data, halo_data_file)
-            halo_data_file.close()
-
-    else:
-        with open('halo_data_appended_d1.txt', 'rb') as halo_data_file:
-            halo_data_appended_d1 = pickle.load(halo_data_file)
-            halo_data_file.close()
-
-# halo_data_appended_trimmed_d1
-# halo_data_appended_trimmed_d2
-# halo_data_appended_trimmed_d3
-# halo_data_appended_trimmed_d4
-# halo_data_appended_trimmed_d5
-
-# halo_data_appended_d1
-# halo_data_appended_d2
-# halo_data_appended_d3
-# halo_data_appended_d4
-# halo_data_appended_d5
-
-print(len(halo_data_appended_d1[0]))
-
 
