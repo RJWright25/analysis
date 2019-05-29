5f6766199567fcaa089f4756f38fd9280fb4f94c
Ruby Wright <21486778@student.uwa.edu.au>
2019-05-29 12:22:57 +0800

    Small changes

--- create_tree_list.py
+++ create_tree_list.py
@@ -1,5 +1,5 @@
 import glob
 import numpy as np
 
-flist=["/mnt/su3ctm/lbakels/CosmRun/9p/Hydro/nonrad/TreeFrogG/tree.snapshot_"+str(i).zfill(3)+".VELOCIraptor.tree" for i in range(201)]
+flist=list(["/mnt/su3ctm/lbakels/CosmRun/9p/Hydro/nonrad/TreeFrogG/tree.snapshot_"+str(i).zfill(3)+".VELOCIraptor.tree" for i in range(201)]).astype(str)
 np.savetxt("/mnt/su3ctm/rwright/hydro_accretion/LB_L32N512/treesnaplist.txt",flist)
