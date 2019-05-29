0008430cadb4ed8a0043f3d1ffdfa3c70498a841
Ruby Wright <21486778@student.uwa.edu.au>
2019-05-29 12:21:09 +0800

    Small changes

--- create_tree_list.py
+++ create_tree_list.py
@@ -2,4 +2,4 @@ import glob
 import numpy
 
 flist=["/mnt/su3ctm/lbakels/CosmRun/9p/Hydro/nonrad/TreeFrogG/tree.snapshot_"+str(i).zfill(3)+".VELOCIraptor.tree" for i in range(201)]
-print(flist)
+np.savetxt("/mnt/su3ctm/rwright/hydro_accretion/LB_L32N512/treesnaplist.txt",flist)
