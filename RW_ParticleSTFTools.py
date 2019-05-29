a4091ec374ea0fd9039e049fdc94a080223ee6fe
Ruby Wright <21486778@student.uwa.edu.au>
2019-05-29 11:12:11 +0800

    Small changes

--- RW_ParticleSTFTools.py
+++ RW_ParticleSTFTools.py
@@ -69,7 +69,7 @@ def read_sim_timesteps(run_directory,sim_type='SWIFT',snap_no=200,files_lz=4):
         for snap in snaps:
             particle_file_temp=h5py.File(particle_data_file_directories[snap])
             for ifield,field in enumerate(fields):
-                print(particle_file_temp['Header'].attrs,fields_out[ifield])
+                print(list(particle_file_temp['Header'].attrs),fields_out[ifield])
                 sim_timesteps[fields_out[ifield]].extend(particle_file_temp['Header'].attrs[field])
             particle_file_temp.close()
 
