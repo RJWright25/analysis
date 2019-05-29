a535de3f55f781e8943162735ca5bf20aef89b08
Ruby Wright <21486778@student.uwa.edu.au>
2019-05-29 12:07:38 +0800

    Small changes

--- create_tree_list.py
+++ create_tree_list.py
@@ -1,5 +1,5 @@
 import glob
 import numpy
 
-print(glob.glob("/mnt/su3ctm/lbakels/CosmRun/9p/Hydro/nonrad/TreeFrogG/*.tree"))
-
+flist=glob.glob("/mnt/su3ctm/lbakels/CosmRun/9p/Hydro/nonrad/TreeFrogG/*.tree")
+numpy.savetxt("/mnt/su3ctm/lbakels/CosmRun/9p/Hydro/nonrad/TreeFrogG/treesnaplist_2.txt",flist)
