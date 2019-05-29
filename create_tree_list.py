d8cc71b5c2b85b3201d5289d5666a7b42e44c6e6
Ruby Wright <21486778@student.uwa.edu.au>
2019-05-29 12:04:58 +0800

    Small changes

--- create_tree_list.py
+++ create_tree_list.py
@@ -1,4 +1,5 @@
-import os
-from os import listdir
+import glob
 import numpy
-print(os.listdir("/mnt/su3ctm/lbakels/CosmRun/9p/Hydro/nonrad/TreeFrogG/"))
+
+print(glob.glob("/mnt/su3ctm/lbakels/CosmRun/9p/Hydro/nonrad/TreeFrogG/*.tree")
+
