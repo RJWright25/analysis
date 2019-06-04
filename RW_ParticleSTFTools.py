db8cd785798bfcd5f87f8fa7ff5301033186b44a
Ruby Wright <21486778@student.uwa.edu.au>
2019-06-04 14:20:34 +0800

    Revamping particle list code

--- RW_ParticleSTFTools.py
+++ RW_ParticleSTFTools.py
@@ -7,6 +7,7 @@ if True:
     import matplotlib.pyplot as plt
     import numpy as np
     import h5py
+    from astropy import constants as const
     from astropy.cosmology import FlatLambdaCDM
 
     # VELOCIraptor python tools 
@@ -42,13 +43,18 @@ def read_sim_timesteps(run_directory,sim_type='SWIFT',snap_no=200,files_lz=4):
 
         for snap in snaps:
             particle_file_temp=h5py.File(particle_data_file_directories[snap])
-            time_unit=particle_file_temp['Units'].attrs['Unit time in cgs (U_t)']
+            time_unit_cgs=particle_file_temp['Units'].attrs['Unit time in cgs (U_t)']
             for ifield,field in enumerate(fields):
                 if ifield==0:
-                    sim_timesteps[fields_out[ifield]].extend(particle_file_temp['Cosmology'].attrs[field]*time_unit/(3600*24*365.25*10**9))
+                    sim_timesteps[fields_out[ifield]].extend(particle_file_temp['Cosmology'].attrs[field]*time_unit_cgs/(const.yr.cgs*10**9))
                 else:
                     sim_timesteps[fields_out[ifield]].extend(particle_file_temp['Cosmology'].attrs[field])
             particle_file_temp.close()
+
+        #mass_unit_cgs=particle_file_temp['Units']['Unit mass in cgs (U_m)']
+
+        #mass table in solar masses
+        #sim_masstable={'M0':particle_file_temp['PartType0']['Masses'][0]*mass_unit_cgs/(const.M_sun.cgs),'M1':particle_file_temp['PartType1']['Masses'][0]*mass_unit_cgs/(const.M_sun.cgs)}
 
     if sim_type=='GADGET':
         fields=['Redshift','Time']
@@ -73,59 +79,17 @@ def read_sim_timesteps(run_directory,sim_type='SWIFT',snap_no=200,files_lz=4):
             particle_file_temp.close()
 
         sim_timesteps['Lookback_time']=cosmo.lookback_time(sim_timesteps['Redshift']).astype(float)
-        return sim_timesteps
 
-def read_swift_particle_data(run_directory,snap_no=200,part_type=[0,1],data_fields=['Coordinates','Masses','ParticleIDs','Velocities'],verbose=1):
-    
-    ##### inputs
-    # run_directory: STRING for directory of run
-    # snap_no: integer number of snaps to record from snap=0
-    # parttype: LIST of INTEGER desired particle type
-    # datafields: LIST of swift particle fields from ['Coordinates','Masses','ParticleIDs','Velocities']
+        #mass_unit_cgs=const.M_sun.cgs*10**10
+        #sim_masstable={'M0':particle_file_temp['Header'].attrs(['MassTable'][0])*mass_unit_cgs/(const.M_sun.cgs),'M1':particle_file_temp['Header'].attrs(['MassTable'][1])*mass_unit_cgs/(const.M_sun.cgs)}
 
-    ##### returns
-    # dictionary (snaps) of dictionaries (corresponding to each parttype) of dictionaries (corresponding to fields) -- masses in solar units, velocities in km/s, positions in Mpc
-    # e.g. particle_data[snap]['PartTypeX']['field'] where X is particle type integer
-
-    snaps=[i for i in range(snap_no)]
-
-    #initialise list
-    particle_data=[[] for i in range(snap_no)]
-    
-    # load hdf5 files
-    particle_data_file_directories=[run_directory+"snap_"+str(snap).zfill(4)+".hdf5" for snap in snaps]
-    particle_data_files=[h5py.File(particle_data_file_directories[index]) for index in range(snap_no)]
-
-    if verbose:
-        print('Saving particle data')
-    
-    index=0
-    # loop over snaps
-    for snap in snaps:
-        particle_data[snap]={}
-        #loop over parttypes
-        for part_type_temp in part_type:
-            particle_data[snap]['PartType'+str(part_type_temp)]={}
-
-            #loop over data fields
-            for field in data_fields:
-                if field=='Masses':
-                    particle_data[snap]['PartType'+str(part_type_temp)][field]=np.array(particle_data_files[index]['PartType'+str(part_type_temp)][field])*10**10
-                else:
-                    particle_data[snap]['PartType'+str(part_type_temp)][field]=np.array(particle_data_files[index]['PartType'+str(part_type_temp)][field])
-        index=index+1
-
-    if verbose:
-        print('Done saving particle data')
-
-    #return dictionary of dictionary of dictionaries
-    return particle_data
+    return sim_timesteps
 
 ##########################################################################################################################################################################
 ############################################################################## CREATE HALO DATA ##########################################################################
 ##########################################################################################################################################################################
 
-def read_vr_treefrog_data(vr_directory,vr_prefix,tf_name,files_type=2,files_nested=False,files_lz=4,snap_no=200,extra_halo_fields=[],halo_TEMPORALHALOIDVAL=1000000,verbose=1):
+def read_vr_treefrog_data(vr_directory,vr_prefix,tf_treefile,vr_files_type=2,vr_files_nested=False,vr_files_lz=4,snap_no=200,extra_halo_fields=[],halo_TEMPORALHALOIDVAL=[],verbose=1):
     # reads velociraptor and treefrog outputs with desired data fields (always includes ['ID','hostHaloID','numSubStruct','Mass_tot','Mass_200crit','M_gas','Xc','Yc','Zc','R_200crit'])
 
     ##### inputs
@@ -146,10 +110,10 @@ def read_vr_treefrog_data(vr_directory,vr_prefix,tf_name,files_type=2,files_nest
         print('Reading halo data using VR python tools')
 
     # Load data from all desired snaps into list structure
-    if files_nested==False:
-        halo_data_all=[ReadPropertyFile(vr_directory+vr_prefix+str(snap).zfill(files_lz),ibinary=files_type,iseparatesubfiles=0,iverbose=0, desiredfields=halo_fields, isiminfo=True, iunitinfo=True) for snap in sim_snaps]
+    if vr_files_nested==False:
+        halo_data_all=[ReadPropertyFile(vr_directory+vr_prefix+str(snap).zfill(vr_files_lz),ibinary=vr_files_type,iseparatesubfiles=0,iverbose=0, desiredfields=halo_fields, isiminfo=True, iunitinfo=True) for snap in sim_snaps]
     else:
-        halo_data_all=[ReadPropertyFile(vr_directory+vr_prefix+str(snap).zfill(files_lz)+"/"+vr_prefix+str(snap).zfill(files_lz),ibinary=files_type,iseparatesubfiles=0,iverbose=0, desiredfields=halo_fields, isiminfo=True, iunitinfo=True) for snap in sim_snaps]
+        halo_data_all=[ReadPropertyFile(vr_directory+vr_prefix+str(snap).zfill(vr_files_lz)+"/"+vr_prefix+str(snap).zfill(vr_files_lz),ibinary=vr_files_type,iseparatesubfiles=0,iverbose=0, desiredfields=halo_fields, isiminfo=True, iunitinfo=True) for snap in sim_snaps]
 
     if verbose==1:
         print('Finished reading halo data')
@@ -162,92 +126,79 @@ def read_vr_treefrog_data(vr_directory,vr_prefix,tf_name,files_type=2,files_nest
     halo_siminfo=[halo_data_all[snap]['SimulationInfo'] for snap in sim_snaps]
     halo_unitinfo=[halo_data_all[snap]['UnitInfo'] for snap in sim_snaps]
 
+    print(halo_siminfo)
+    print(halo_unitinfo)
+    
     # import tree data from TreeFrog, build temporal head/tails from descendants -- adds to halo_data_all (all halo data)
     if verbose==1:
         print('Assembling descendent tree using VR python tools')
 
-    tf_treefile=tf_name
-    halo_tree=ReadHaloMergerTreeDescendant(tf_treefile,ibinary=files_type,iverbose=1,imerit=True,inpart=False)
-    BuildTemporalHeadTailDescendant(snap_no,halo_tree,halo_data_counts,halo_data_all,iverbose=0)
+    halo_tree=ReadHaloMergerTreeDescendant(tf_treefile,ibinary=vr_files_type,iverbose=1,imerit=True,inpart=False)
+    if halo_TEMPORALHALOIDVAL==[]:
+        BuildTemporalHeadTailDescendant(snap_no,halo_tree,halo_data_counts,halo_data_all,iverbose=0)
+    else:
+        BuildTemporalHeadTailDescendant(snap_no,halo_tree,halo_data_counts,halo_data_all,iverbose=0,TEMPORALHALOIDVAL=halo_TEMPORALHALOIDVAL)
     
     if verbose==1:
         print('Finished assembling descendent tree using VR python tools')
-    
-    return halo_data_all
-
-def add_particle_lists(vr_directory,vr_prefix,halo_data_all,files_type=2,files_nested=False,files_lz=4,part_data_from_snap=120,verbose=1):
-    snap_no=len(halo_data_all)
-    
-    #save the no_pl data
-    if verbose==1:
-        print('Adding particle lists to halos using VR python tools')
-    
-    for snap in range(snap_no): #iterate through snaps to add particle data to halo_data_all structure
-
-        if snap>part_data_from_snap:#if there are a good amount of halos and snap late enough
-            if verbose==True:
-                print('Adding particle lists to halos for snap = ',snap)
-
-            n_halos=len(halo_data_all[snap]['ID'])
-
-            if files_nested==False:
-                part_data_temp=ReadParticleDataFile(vr_directory+vr_prefix+str(snap).zfill(files_lz),ibinary=files_type,iverbose=0,iparttypes=1)
-            else:
-                part_data_temp=ReadParticleDataFile(vr_directory+vr_prefix+str(snap).zfill(files_lz)+"/"+vr_prefix+str(snap).zfill(files_lz),ibinary=files_type,iverbose=0,iparttypes=1)
-
-            for part_key in list(part_data_temp.keys()):
-                halo_data_all[snap][part_key]=part_data_temp[part_key]
-
-        else:#if we don't have particle data yet
-            part_data_temp={"Npart":[],"Npart_unbound":[],'Particle_IDs':[],'Particle_Types':[]}
-            for part_key in list(part_data_temp.keys()):
-                halo_data_all[snap][part_key]=part_data_temp[part_key] 
-
-
-    if verbose==1:
-        print('Finished adding particle lists to halos using VR python tools')
-
-    if verbose==1:
-        print('Appending FOF particle lists with substructure')
-
-    for snap in range(snap_no):#iterate through snaps to add substructure particle lists to FOF halo particle
-
-        if snap>part_data_from_snap:
-            if verbose==1:
-                print('Adding substructure particles to FOF halos for snap = ',snap)
-
-            halo_data_temp=halo_data_all[snap]
-            field_halo_indices_temp=np.where(halo_data_temp['hostHaloID']==-1)[0]#find field/fof halos
-
-            if len(field_halo_indices_temp)>0:#where there are field halos
-                for field_halo_ID in halo_data_temp['ID'][field_halo_indices_temp]:#go through each field halo
-
-                    sub_halos_temp=(np.where(halo_data_temp['hostHaloID']==field_halo_ID)[0])#find its subhalos
-
-                    if len(sub_halos_temp)>0:#where there is substructure
-
-                        field_halo_temp_index=np.where(halo_data_temp['ID']==field_halo_ID)[0][0]
-                        field_halo_plist=halo_data_temp['Particle_IDs'][field_halo_temp_index]
-                        field_halo_tlist=halo_data_temp['Particle_Types'][field_halo_temp_index]
-                        sub_halos_plist=np.concatenate([halo_data_temp['Particle_IDs'][isub] for isub in sub_halos_temp])#list all particles IDs in substructure
-                        sub_halos_tlist=np.concatenate([halo_data_temp['Particle_Types'][isub] for isub in sub_halos_temp])#list all particles types substructure
-
-                        halo_data_temp['Particle_IDs'][field_halo_temp_index]=np.concatenate([field_halo_plist,sub_halos_plist])#add particles to field halo particle list
-                        halo_data_temp['Particle_Types'][field_halo_temp_index]=np.concatenate([field_halo_tlist,sub_halos_tlist])#add particles to field halo particle list
-                        halo_data_temp['Npart'][field_halo_temp_index]=len(halo_data_temp['Particle_IDs'][field_halo_temp_index])#update Npart for each field halo
-
-    if verbose==1:
-        print('Finished appending FOF particle lists with substructure')
 
     return halo_data_all
+
+def gen_particle_lists(snap,halo_data_snap,vr_directory,vr_prefix,vr_files_type=2,vr_files_nested=False,vr_files_lz=4,verbose=1):
+    
+    if verbose==True:
+        print('Reading particle lists for snap = ',snap)
+
+    try:
+        if vr_files_nested==False:
+            part_data_temp=ReadParticleDataFile(vr_directory+vr_prefix+str(snap).zfill(vr_files_lz),ibinary=vr_files_type,iverbose=0,iparttypes=1)
+            if verbose:
+                print('Particle data found for snap = ',snap)
+        else:
+            part_data_temp=ReadParticleDataFile(vr_directory+vr_prefix+str(snap).zfill(vr_files_lz)+"/"+vr_prefix+str(snap).zfill(vr_files_lz),ibinary=vr_files_type,iverbose=0,iparttypes=1)
+            if verbose:
+                print('Particle data found for snap = ',snap)
+
+    except:#if we can't load particle data
+        if verbose:
+            print('Particle data not found for snap = ',snap)
+        part_data_temp={"Npart":[],"Npart_unbound":[],'Particle_IDs':[],'Particle_Types':[]}
+        return part_data_temp
+
+    if verbose==1:
+        print('Appending FOF particle lists with substructure for snap = ',snap)
+
+    field_halo_indices_temp=np.where(halo_data_snap['hostHaloID']==-1)[0]#find field/fof halos
+
+        if len(field_halo_indices_temp)>0:#where there are field halos
+            for field_halo_ID in halo_data_temp['ID'][field_halo_indices_temp]:#go through each field halo
+
+                sub_halos_temp=(np.where(halo_data_snap['hostHaloID']==field_halo_ID)[0])#find its subhalos
+
+                if len(sub_halos_temp)>0:#where there is substructure
+
+                    field_halo_temp_index=np.where(halo_data_snap['ID']==field_halo_ID)[0][0]
+                    field_halo_plist=part_data_temp['Particle_IDs'][field_halo_temp_index]
+                    field_halo_tlist=part_data_temp['Particle_Types'][field_halo_temp_index]
+                    sub_halos_plist=np.concatenate([part_data_temp['Particle_IDs'][isub] for isub in sub_halos_temp])#list all particles IDs in substructure
+                    sub_halos_tlist=np.concatenate([part_data_temp['Particle_Types'][isub] for isub in sub_halos_temp])#list all particles types substructure
+
+                    part_data_temp['Particle_IDs'][field_halo_temp_index]=np.concatenate([field_halo_plist,sub_halos_plist])#add particles to field halo particle list
+                    part_data_temp['Particle_Types'][field_halo_temp_index]=np.concatenate([field_halo_tlist,sub_halos_tlist])#add particles to field halo particle list
+                    part_data_temp['Npart'][field_halo_temp_index]=len(halo_data_temp['Particle_IDs'][field_halo_temp_index])#update Npart for each field halo
+
+    if verbose==1:
+        print('Finished with particle lists for snap = ',snap)
+
+    return part_data_temp
 
 ##########################################################################################################################################################################
 ############################################################################## CREATE PARTICLE HISTORIES #################################################################
 ##########################################################################################################################################################################
 
-def gen_part_history(halo_data,verbose=True):
+def gen_part_history(halo_data,vr_directory,vr_prefix,vr_files_type=2,vr_files_nested=False,vr_files_lz=4,verbose=1):
     ##### inputs
-    # halo_data (from above - needs particle lists)
+    # halo_data (from above)
 
     ##### returns
     # list (for each snap) of dictionaries (sub/field ids and types) of lists (for each bound particle) of:
@@ -259,10 +210,11 @@ def gen_part_history(halo_data,verbose=True):
 
     for snap in range(len(halo_data)):
 
-        if len(halo_data[snap]['Particle_IDs'])==0:#if no halos
+        particle_data_temp=read_particle_lists(snap=snap,halo_data_snap=halo_data[snap],vr_directory=vr_directory,vr_prefix=vr_prefix,vr_files_type=vr_files_type,vr_files_nested=vr_files_nested)
+
+        if len(particle_data_temp['Particle_IDs'])==0:#if no halos or no particle data
             field_part_ids[snap]=[]
             sub_part_ids[snap]=[]
-
 
         else:
             if verbose==True:
@@ -272,8 +224,8 @@ def gen_part_history(halo_data,verbose=True):
             sub_halo_indices=np.where(halo_data[snap]['hostHaloID']>0)[0]
 
             #list of particles in field/subhalos for this snap
-            field_part_ids_temp=[halo_data[snap]['Particle_IDs'][field_halo_index] for field_halo_index in field_halo_indices]
-            sub_part_ids_temp=[halo_data[snap]['Particle_IDs'][sub_halo_index] for sub_halo_index in sub_halo_indices]
+            field_part_ids_temp=[particle_data_temp['Particle_IDs'][field_halo_index] for field_halo_index in field_halo_indices]
+            sub_part_ids_temp=[particle_data_temp['Particle_IDs'][sub_halo_index] for sub_halo_index in sub_halo_indices]
 
             field_part_ids_flattened=[]
             sub_part_ids_flattened=[]
@@ -297,12 +249,11 @@ def gen_part_history(halo_data,verbose=True):
     print('Unique particle histories created')
     return [{"field_ids":field_part_ids_unique[snap],"sub_ids":sub_part_ids_unique[snap]} for snap in range(len(halo_data))]
 
-
 ##########################################################################################################################################################################
 ############################################################################## CALC DELTA_N ##############################################################################
 ##########################################################################################################################################################################
 
-def gen_delta_npart(halo_data,sim_timesteps,type_order,mass_table,unique_particle_list=[],depth=5,trim_hoes=True,verbose=True): 
+def gen_delta_npart(halo_data,unique_particle_list,sim_timesteps,depth=5,trim_hoes=True,verbose=True): 
 
     ##### inputs
     # halo_data (from above - needs particle lists)
