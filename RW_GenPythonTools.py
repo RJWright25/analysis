97708e9c42ad19f6bed734f4383f3f710b5a6ee0
Ruby Wright <21486778@student.uwa.edu.au>
2019-06-19 16:46:20 +0800

    Small changes

--- RW_GenPythonTools.py
+++ RW_GenPythonTools.py
@@ -128,7 +128,7 @@ def bin_xy(x,y,bins='eq',y_lop=16,y_hip=84,bin_min=5,verbose=False):
             n_h=b-n_l
             n=concatenate([ones(ceil(n_l/2))*w_l,ones(n_h)*w_h,ones(floor(n_l/2))*w_l]).astype(int)
             print(n)
-            bin_edges=array([nanmin(x_forbins)]+[(x_forbins[i-1]+x_forbins[i])/2 for i in cumsum(n)[:-1]]+[nanmax(x_forbins)])
+            bin_edges=array([np.nanmin(x_forbins)]+[(x_forbins[i-1]+x_forbins[i])/2 for i in cumsum(n)[:-1]]+[np.nanmax(x_forbins)])
             bin_mid=np.array([bin_edges[i]+bin_edges[i+1] for i in range(bin_no)])*0.5
         if verbose:
             print("Bins generated, xmin = ",bin_edges[0],', xmax = ',bin_edges[-1],' and bin no = ',bin_no)
