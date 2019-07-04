df64888b948b9ff6b705276c323afa49b7a7f90e
Ruby Wright <21486778@student.uwa.edu.au>
2019-07-05 07:52:00 +0800

    Small changes

--- RW_GenPythonTools.py
+++ RW_GenPythonTools.py
@@ -183,7 +183,8 @@ def bin_xy(x,y,xy_mask=[],bins='eq',bin_range=[],n_per_bin=100,y_lop=16,y_hip=84
         bin_mask=np.logical_and(x>bin_lo,x<bin_hi)#mask for all points within x bin
         bin_count_gross=np.nansum(bin_mask)
         bin_count=bin_count_gross#count of selected objects
-
+        print(bin_count)
+        
         x_subset=np.compress(bin_mask,np.array(x))
         y_subset=np.compress(bin_mask,np.array(y))
 
