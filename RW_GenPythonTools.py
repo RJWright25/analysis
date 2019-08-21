5610929a877514de249deceec856d3fa455c82a0
Ruby Wright <21486778@student.uwa.edu.au>
2019-08-22 05:50:33 +0800

    Small changes to gen_particle_histories

--- RW_GenPythonTools.py
+++ RW_GenPythonTools.py
@@ -238,6 +238,76 @@ def binary_search_2(element,sorted_array, lo=0, hi=None):   # can't use a to spe
 
 
 
+########################### INDEX LISTS GENERATOR FOR MP ###########################
+
+def gen_indices_mp(index_list,n_processes):
+    """
+
+    gen_halo_indices_mp : function
+	----------
+
+    Generate list of lists of desired indices divided amongst a given amount of processes.
+
+	Parameters
+	----------
+    index_list : list or int
+        If list, a list of integer halo indices to divide.
+        If int, a list of integer halo indices up to the int is generated.
+
+    n_processes : int
+        Number of processes (likely number of cores) to distribute halo indices across. 
+
+    Returns
+	----------
+    output_index_lists : list of lists
+        The resulting halo index lists for each process. 
+
+    """
+    # Create halo index list from integer or provided list
+    if type(index_list)==int:
+        index_list=list(range(index_list))
+    else:
+        index_list=list(index_list)
+
+    n_indices=len(index_list)
+    num_rem=n_halos%n_processes
+    n_indices_per_process=int(n_indices/n_processes)
+
+    #initialising loop variables
+    last_index=0
+    i_index_lists=[]
+    output_index_lists=[]
+
+    #loop for each process to generate halo index lists
+    for iprocess in range(n_processes):
+        if num_rem==0: #if there's an exact multiple of halos as cpu cores then distribute evenly
+            indices_temp=list(range(iprocess*n_indices_per_process,(iprocess+1)*n_indices_per_process))
+            i_index_lists.append(indices_temp)
+            index_list_temp=[index_list[index_temp] for index_temp in indices_temp]
+            output_index_lists.append(index_list_temp)
+
+        else: #otherwise split halos evenly except last process
+            if iprocess<num_rem:
+                indices_temp=list(range(last_index,last_index+n_indices_per_process+1))
+                i_index_lists.append(indices_temp)
+                last_index=indices_temp[-1]+1
+                index_list_temp=[all_[index_temp] for index_temp in indices_temp]
+                output_index_lists.append(halo_index_list_temp)
+
+            else:
+                indices_temp=list(range(last_index,last_index+n_halos_per_process))
+                i_index_lists.append(indices_temp)
+                last_index=indices_temp[-1]+1
+                index_list_temp=[all_halo_indices[index_temp] for index_temp in indices_temp]
+                output_index_lists.append(halo_index_list_temp)
+
+    return output_index_lists
+
+
+
+
+
+
 
 
 ######### plotting common axes #########
